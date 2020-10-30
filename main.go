21181d0465532ec2f454343c0b5cff7944bfe687
Lawrence Weikum <lawrencemq@gmail.com>
2020-10-30 10:14:45 -0700

    Initial hashtag generator complete

--- main.go
+++ main.go
@@ -1,16 +1,108 @@
 package main
 
 import (
+	"net/http"
+	"fmt"
+	"regexp"
+	"strings"
 	"github.com/AlecAivazis/survey/v2"
+	"github.com/PuerkitoBio/goquery"
 )
+
+var blockedTags  = map[string]bool{
+	"#bhfyp": true,
+	"#ol": true,
+	"#follow": true,
+	"#me": true,
+	"#love": true,
+	"#like": true,
+}
+
+func getTagsAtUrl(url string) []string {
+	
+	fmt.Printf("Getting %s...\n", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	
+	document, err := goquery.NewDocumentFromReader(resp.Body)
+    if err != nil {
+       panic(err)
+    }
+
+	found := document.Find(".tag-box")
+
+	return strings.Fields(found.Text())
+
+}
+
+func createUrlForHashtag(tag string) string {
+	template := "http://best-hashtags.com/hashtag/%s/"
+	
+	 // only want letters and numbers
+	 reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	 if err != nil {
+		 panic(err)
+	 }
+	 cleanedTag := reg.ReplaceAllString(tag, "")
+	
+	return fmt.Sprintf(template, cleanedTag)
+}
+
+
+func getDataForHashtags(tags []string) []string {
+
+	foundTags := make(map[string]bool)
+
+	// Gathering all tags from HTMLs
+	for _, tag := range tags {
+		tagList := getTagsAtUrl(createUrlForHashtag(tag))
+		for _, t := range tagList{
+			foundTags[t] = true
+		}
+		
+	}
+
+	// Removing banned tags
+	for t := range blockedTags{
+		if _, ok := foundTags[t]; ok {
+			delete(foundTags, t)
+		}
+	}
+
+	finalTags := make([]string, 0, len(foundTags))
+    for k := range foundTags {
+        finalTags = append(finalTags, k)
+    }
+
+	return finalTags
+}
 
 func main() {
 
-	days := []string{}
-	prompt := &survey.MultiSelect{
-		Message: "What days do you prefer:",
-		Options: []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"},
+	var hashtagsToSearch = []string{}
+	for true{
+		tag := ""
+		prompt := &survey.Input{
+			Message: "ping",
+		}
+		survey.AskOne(prompt, &tag)
+		if len(tag) == 0 {
+			break
+		}
+		hashtagsToSearch = append(hashtagsToSearch, tag)
 	}
-	survey.AskOne(prompt, &days)
+	
+	
+	hashtagList := getDataForHashtags(hashtagsToSearch)
+
+	hashtagsToKeep := []string{}
+	prompt := &survey.MultiSelect{
+		Message: "Which hashtags shal be kept:",
+		Options: hashtagList,
+	}
+	survey.AskOne(prompt, &hashtagsToKeep)
 
 }
