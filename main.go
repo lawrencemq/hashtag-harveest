88b807de7800be52b2a558b262415315f0e583fe
Lawrence Weikum <lawrencemq@gmail.com>
2021-01-07 13:47:26 -0800

    using simple maps

--- main.go
+++ main.go
@@ -1,49 +1,47 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 	"regexp"
 	"strings"
+
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/PuerkitoBio/goquery"
-	mapset "github.com/deckarep/golang-set"
 )
 
-var blockedTags = []interface{}{
-	"#bhfyp",
-	"#ol",
-	"#follow",
-	"#me",
-	"#love",
-	"#like",
+var blockedTags = []string{
+	"bhfyp",
+	"ol",
+	"follow",
+	"me",
+	"love",
+	"like",
 }
-var blockedTagsSet = mapset.NewSetFromSlice(blockedTags)
 
-var requiredTags = []interface{} {
-	"#tiktok",
-	"#9gag",
-	"#meme",
-	"#memes",
-	"#dailymemes",
-	"#memesdaily",
-	"#memepage",
+var requiredTags = []string{
+	"tiktok",
+	"9gag",
+	"meme",
+	"memes",
+	"dailymemes",
+	"memesdaily",
+	"memepage",
 }
-var requiredTagsSet = mapset.NewSetFromSlice(requiredTags)
 
 func getTagsAtUrl(url string, tagChannel chan []string) {
-	
+
 	fmt.Printf("Getting %s...\n", url)
 	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	
+
 	document, err := goquery.NewDocumentFromReader(resp.Body)
-    if err != nil {
-       panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
 	found := document.Find(".tag-box")
 
@@ -52,61 +50,63 @@ func getTagsAtUrl(url string, tagChannel chan []string) {
 
 func createUrlForHashtag(tag string) string {
 	template := "http://best-hashtags.com/hashtag/%s/"
-	
-	 // only want letters and numbers
-	 reg, err := regexp.Compile("[^a-zA-Z0-9]+")
-	 if err != nil {
-		 panic(err)
-	 }
-	 cleanedTag := reg.ReplaceAllString(tag, "")
-	
+
+	// only want letters and numbers
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	if err != nil {
+		panic(err)
+	}
+	cleanedTag := reg.ReplaceAllString(tag, "")
+
 	return fmt.Sprintf(template, cleanedTag)
 }
-
 
 func getDataForHashtags(tags []string) []string {
 
 	dataChannel := make(chan []string)
 
-	foundTags := mapset.NewSet()
+	foundTags := map[string]bool{}
+
+	// Adding requested tags
+	for _, tag := range tags {
+		foundTags[tag] = true
+	}
 
 	// Gathering all tags from HTMLs
 	for _, tag := range tags {
 		go getTagsAtUrl(createUrlForHashtag(tag), dataChannel)
 	}
 
-	for  i := 0; i < len(tags); i++{
-		tagList :=  <- dataChannel
-		for _, t := range tagList{
-			foundTags.Add(t)
+	for i := 0; i < len(tags); i++ {
+		tagList := <-dataChannel
+		for _, t := range tagList {
+			foundTags[t[1:]] = true
 		}
 	}
 
 	// Removing banned tags and constant tags
-	foundTags = foundTags.Difference(blockedTagsSet)
-	foundTags = foundTags.Difference(requiredTagsSet)
-	
-	tagsToReturn := make([]string, foundTags.Cardinality())
-	for i, v := range foundTags.ToSlice() {
-		tagsToReturn[i] = v.(string)
+	for _, tag := range blockedTags {
+		delete(foundTags, tag)
 	}
 
-	return tagsToReturn
-}
-
-func setToStrings(set mapset.Set) []string {
-	tagsToReturn := make([]string, set.Cardinality())
-	for i, v := range set.ToSlice() {
-		tagsToReturn[i] = v.(string)
+	// Removing required tags as they're added in later
+	for _, tag := range requiredTags {
+		delete(foundTags, tag)
 	}
-	return tagsToReturn
 
+	// Returning just the tags
+	finalTags := make([]string, 0, len(foundTags))
+	for tag := range foundTags {
+		finalTags = append(finalTags, tag)
+	}
+
+	return finalTags
 }
 
 func main() {
 
 	var hashtagsToSearch = []string{}
-	for true{
+	for true {
 		tag := ""
 		prompt := &survey.Input{
 			Message: "Enter new hashtag to search (enter none to stop)",
@@ -117,18 +117,16 @@ func main() {
 		}
 		hashtagsToSearch = append(hashtagsToSearch, tag)
 	}
-	
-	
+
 	hashtagList := getDataForHashtags(hashtagsToSearch)
 
 	hashtagsToKeep := []string{}
 	prompt := &survey.MultiSelect{
-		Message: "Which hashtags shal be kept:",
+		Message: "Which hashtags shall be kept:",
 		Options: hashtagList,
 	}
 	survey.AskOne(prompt, &hashtagsToKeep)
 
-
-	fmt.Printf("Hashtags: %s %s %s", strings.Join(hashtagsToSearch, " "), strings.Join(setToStrings(requiredTagsSet), " "), strings.Join(hashtagsToKeep, " "))
+	fmt.Printf("Hashtags: #%s #%s #%s", strings.Join(hashtagsToSearch, " #"), strings.Join(requiredTags, " #"), strings.Join(hashtagsToKeep, " #"))
 
 }
